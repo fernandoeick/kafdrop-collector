5314c74731f2a9a0b108a09ab7e234d8d688b09d
Fernando Eickhoff <fernando.eickhoff@sensedia.com>
2021-03-10 18:07:30 -0300

    kafdrop and kafka metrics collector

--- kafdrop-collector.py
+++ kafdrop-collector.py
@@ -3,90 +3,94 @@ import json
 import sys
 
 Kafdrop = sys.argv[1]
-brokersSize = {}
-brokersTopics = {}
 
-def setBrokers():
+topicPartitionsDict = {}
+topicReplicasDict = {}
+topicDict = {}
+
+def readAllBrokers():
     brokers = requests.get(Kafdrop + "broker")
     allBrokers = json.loads(brokers.text)
-    
     if allBrokers:
-        for broker in allBrokers:
+        for idx, broker in enumerate(allBrokers):
             brokerId = str(broker['id'])
             if brokerId:
                 requestURL = Kafdrop + "broker/" + brokerId
-                brokerDetails = requests.get(requestURL, headers={'accept': 'application/json'})
-                brokerDetailsResult = json.loads(brokerDetails.text)
-                brokersSize[brokerId] = 0
-                brokersTopics[brokerId] = ""
+                brokersJson = requests.get(requestURL, headers={'accept': 'application/json'})
+                brokersText = json.loads(brokersJson.text)
+                topicReplicasDict.update({brokerId : 0})
+                topicPartitionsDict.update({brokerId : 0})
+                topicDict.update({brokerId : 0})
     else:
         print("No brokers were found!")
 
-def getTopicsPartitionsByBroker():
+def readTopicsAndPartitions():
     topics = requests.get(Kafdrop +  "topic")
     allTopics = json.loads(topics.text)
-
     if allTopics:
         for topic in allTopics:
-            metricExpression = ""
-
+            leaderId = ""
             topicName = str(topic['name'])
-            metricExpression = metricExpression + "topicName:" + topicName + ";"
-
             if not(topicName.startswith("__")):
                 requestURL = Kafdrop +  "topic/" + topicName
                 topicDetails = requests.get(requestURL, headers={'accept': 'application/json'})
                 topicDetailsResult = json.loads(topicDetails.text)
-
                 partitions = topicDetailsResult['partitions']
+
                 for partition in partitions:
                     partitionId = str(partition['id'])
                     leader = partition['leader']
                     leaderId = str(leader['id'])   
-
-                    metricExpression = metricExpression + "partitionId:" + partitionId + ";leaderId:" + leaderId + ";"
-
                     replicas = partition['replicas']
+                    topicPartitionsDict.update({leaderId : topicPartitionsDict[leaderId]+1})
                     for replica in replicas:
-                        broker = str(replica['id'])
-                        brokersSize[broker] += 1; #Fix: é o ID do broker aqui e não a posição do array
-                        brokersTopics[broker] += topicName + ";"
-                        metricExpression = metricExpression + "brokerId:" + broker + ";"
+                        brokerId = str(replica['id'])
+                        topicReplicasDict.update({brokerId : topicReplicasDict[brokerId]+1})
 
-                print(metricExpression)
-
+                    topicDict.update({leaderId : topicPartitionsDict[leaderId]+1})
     else:
         print("No topics were found!")
 
-def getPartitionsByBroker():
-    metricExpression = ""
-    
-    for broker, value in brokersSize.items():
-        print("brokerId:" + broker + ";" + "partitions:" + str(value))
+def printReplicasByBroker():
+    for key, value in topicReplicasDict.items():
+        metricExpression = "aws.kafka.kafdrop.topic_replicas: "
+        metricExpression = metricExpression + str(value) + "; broker_id: " + key + ", cluster_name: " + "xpto" + ", env: " + "xpto" + ", region: " + "xpto" + ", availability_zone: " + "xpto"
+        print(metricExpression)
 
-    print(metricExpression)
+def printPartitionsByBroker():
+    for key, value in topicPartitionsDict.items():
+        metricExpression = "aws.kafka.kafdrop.topic_partitions: "
+        metricExpression = metricExpression + str(value) + "; broker_id: " + key + ", cluster_name: " + "xpto" + ", env: " + "xpto" + ", region: " + "xpto" + ", availability_zone: " + "xpto"
+        print(metricExpression)
 
-def getMessagesByTopic():
+def printTopicsByBroker():
+    for key, value in topicDict.items():
+        metricExpression = "aws.kafka.kafdrop.broker_topics: "
+        metricExpression = metricExpression + str(value) + "; broker_id: " + key + ", cluster_name: " + "xpto" + ", env: " + "xpto" + ", region: " + "xpto" + ", availability_zone: " + "xpto"
+        print(metricExpression)
+
+def printMessagesByTopic():
     topics = requests.get(Kafdrop +  "topic")
     allTopics = json.loads(topics.text)
-
-    metricExpression = ""
-
     for topic in allTopics:
         topicName = str(topic['name'])
-        requestURL = Kafdrop +  "topic/" + topicName + "/messages"
-        messages = requests.get(requestURL, headers={'accept': 'application/json'})
-        messagesResult = json.loads(messages.text)
-        lastOffSet = messagesResult[0]['lastOffset']
-        metricExpression = "topicName:" + topicName + ";messagesCount:" + str(lastOffSet) + ";"
-        print(metricExpression)
-    
+        if not(topicName.startswith("__")):
+            metricExpression = "aws.kafka.kafdrop.topic_messages: "
+            topicName = str(topic['name'])
+            requestURL = Kafdrop +  "topic/" + topicName + "/messages"
+            messagesJson = requests.get(requestURL, headers={'accept': 'application/json'})
+            messagesText = json.loads(messagesJson.text)
+            lastOffSet = messagesText[0]['lastOffset']
+            metricExpression = metricExpression + str(lastOffSet)  + "; topic_name: " + topicName + ", cluster_name: " + "xpto" + ", env: " + "xpto" + ", region: " + "xpto" + ", availability_zone: " + "xpto"
+            print(metricExpression)
 
 def main():
-    setBrokers()
-    getTopicsPartitionsByBroker()
-    getPartitionsByBroker()
-    getMessagesByTopic()
+    readAllBrokers()
+    readTopicsAndPartitions()
+    printReplicasByBroker()
+    printPartitionsByBroker()
+    printTopicsByBroker()
+    printMessagesByTopic()
 
 if __name__ == "__main__":
     main()
