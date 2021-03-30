97bd476fed34682c9e46fafc9c8ced5c05a606d4
Fernando Eickhoff <fernando.eickhoff@sensedia.com>
2021-03-29 23:49:45 -0300

    general improvments

--- kafdrop-installation/files/kafdrop_run.sh
+++ kafdrop-installation/files/kafdrop_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -jar kafdrop.jar --kafka.brokerConnect=b-2.demo-cluster.o3pz5h.c2.kafka.us-east-2.amazonaws.com:9094,b-3.demo-cluster.o3pz5h.c2.kafka.us-east-2.amazonaws.com:9094,b-4.demo-cluster.o3pz5h.c2.kafka.us-east-2.amazonaws.com:9094
+java --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -jar /usr/local/bin/kafdrop/kafdrop.jar --kafka.brokerConnect=b-2.demo-cluster.o3pz5h.c2.kafka.us-east-2.amazonaws.com:9094,b-3.demo-cluster.o3pz5h.c2.kafka.us-east-2.amazonaws.com:9094,b-4.demo-cluster.o3pz5h.c2.kafka.us-east-2.amazonaws.com:9094
