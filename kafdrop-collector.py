12f913f87d1ca857efce4967c87636c340f63674
Fernando Eickhoff <fernando_eickhoff@Fernandos-MacBook-Pro.local>
2021-04-24 14:26:18 -0300

    ïmprove metrics data

--- kafdrop-collector.py
+++ kafdrop-collector.py
@@ -2,15 +2,14 @@ import requests
 import json
 import sys
 import logging
-from datadog import initialize, statsd, api
+from datadog import initialize, statsd
 import time
 
 kafdropAddress = sys.argv[1]
 brokerMetrics = {}
 options = {
-    #'statsd_host':'127.0.0.1',
-    #'statsd_port':8125
-    'api_key': 'c79379bc72cface84a42bcb603d73eb3'
+    'statsd_host':'127.0.0.1',
+    'statsd_port':8125
 }
 
 initialize(**options)
@@ -21,27 +20,8 @@ def defineLogConfiguration():
     logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S', level=logging.INFO)
 
-def readAllBrokers():
-    try:
-        brokers = requests.get(kafdropAddress + "broker")
-        allBrokers = json.loads(brokers.text)
-        
-        if allBrokers:
-            for idx, broker in enumerate(allBrokers):
-                brokerId = str(broker['id'])
-                if brokerId:
-                    requestURL = kafdropAddress + "broker/" + brokerId
-                    brokersJson = requests.get(requestURL, headers={'accept': 'application/json'})
-                    brokersText = json.loads(brokersJson.text)
-                    brokerMetric = ({"brokerId": brokerId, "topicsName": [], "partitionIds": {}, "topicPartitions" : 0, "partitionsReplicas" : 0, "host" : broker['host'], "rack" : "N/A" if broker['rack']==None else broker['rack']})
-                    brokerMetrics[idx] = brokerMetric
-        else:
-            logging.warning("No brokers were found!")
-    except:
-        logging.error("It was not possible to connect with the MSK cluster.")
-        exit()
-
-def readTopicsAndPartitions():
+def loadTopicMessagesMetric():
+    topicMessagesMetricName = "tr.taxprof.kafkaadmin.topic.messages"
     try:
         topics = requests.get(kafdropAddress +  "topic")
         allTopics = json.loads(topics.text)
@@ -49,152 +29,46 @@ def readTopicsAndPartitions():
         if allTopics:
             for topic in allTopics:
                 topicName = str(topic['name'])
-
-                #if not(topicName.startswith("__")):
                 requestURL = kafdropAddress +  "topic/" + topicName
-                topicDetailsJson = requests.get(requestURL, headers={'accept': 'application/json'})
-                topicDetailsText = json.loads(topicDetailsJson.text)
-                partitions = topicDetailsText['partitions']
+                topicDetails = requests.get(requestURL, headers={'accept': 'application/json'})
+                topicDetailsStr = json.loads(topicDetails.text)
+                partitions = topicDetailsStr['partitions']
 
                 if partitions:
                     for partition in partitions:
+                        requestURL = kafdropAddress +  "topic/" + topicName + "/messages"
+                        messages = requests.get(requestURL, headers={'accept': 'application/json'})
+                        messagesStr = json.loads(messages.text)
+                        lastOffSet = messagesStr[0]['lastOffset']
+
+                        brokerId = str(partition['leader']['id'])
                         partitionId = str(partition['id'])
-                        leader = partition['leader']
-                        leaderId = str(leader['id'])   
-                            
-                        idx = getIdx(leaderId)
-                        brokerMetrics[idx]["topicPartitions"] = brokerMetrics[idx]["topicPartitions"] + 1
-                        lastPartitionPosition = len(brokerMetrics[idx]["partitionIds"])
-                        brokerMetrics[idx]["partitionIds"][lastPartitionPosition] = partitionId
-                        
-                        if(topicName not in brokerMetrics[idx]["topicsName"]):
-                            lastTopicNamePosition = len(brokerMetrics[idx]["topicsName"])
-                            brokerMetrics[idx]["topicsName"].insert(lastTopicNamePosition, topicName)
+                        lastOffSet = lastOffSet
+                        underReplicated = str(partition['underReplicated'])
+                        clusterName = "my_cluster"
 
-                        replicas = partition['replicas']
-                        for replica in replicas:
-                            brokerId = str(replica['id'])
-                            idx = getIdx(brokerId)
-                            brokerMetrics[idx]["partitionsReplicas"] = brokerMetrics[idx]["partitionsReplicas"] + 1
+                        topicMessagesMetricTags = [
+                            "broker_id:"+brokerId,
+                            "partition_id:"+partitionId,
+                            "last_offset:"+lastOffSet,
+                            "is_under_replicated:"+underReplicated,
+                            "cluster_name:"+clusterName])
+                        ]
 
+                        statsd.increment(topicMessagesMetricName, tags=topicMessagesMetricTags
+                        logging.info("metricName: " + topicMessagesMetricTags + "metricTags: "+ topicMessagesMetricTags)
                 else:
-                    logging.warning("No partitions were found!")
-
+                    logging.warning("No partitions were found in topic " + topicName)
         else:
             logging.warning("No topics were found!")
 
-    except:
-        logging.error("It was not possible to connect with the MSK cluster.")
-        exit()
-
-def getIdx(brokerIdToFind):
-    for idx, metricDict in brokerMetrics.items():
-        brokerId = brokerMetrics[idx]["brokerId"]
-        if(brokerId == brokerIdToFind):
-            return idx
-
-def printMetrics():
-
-    if brokerMetrics:
-        for idx, metricDict in brokerMetrics.items():
-            brokerId = brokerMetrics[idx]["brokerId"]
-            topicNames = brokerMetrics[idx]["topicsName"]
-            host = brokerMetrics[idx]["host"]
-            rack = brokerMetrics[idx]["rack"]
-
-            metricName = "aws.kafka.kafdrop.broker_topics:"
-            metricTags = "topics_number: " + str(len(topicNames)) + ", broker_id: " + brokerId + ", cluster_hostname: " + host + ", availability_zone: " + rack + ","
-            #statsd.increment(metricName, tags=[metricTags])
-            api.Event.create(title="kafdrop-metrics", text=metricName, tags=metricTags)
-            logging.info(metricTags)
-            time.sleep(3)
-
-        for idx, metricDict in brokerMetrics.items():
-            brokerId = brokerMetrics[idx]["brokerId"]
-            topicPartitions = brokerMetrics[idx]["topicPartitions"]
-            host = brokerMetrics[idx]["host"]
-            rack = brokerMetrics[idx]["rack"]
-
-            metricName = "aws.kafka.kafdrop.broker_partition:"
-            metricTags = "partitions_number:" + str(topicPartitions) + ", broker_id: " + brokerId + ", cluster_hostname: " + host + ", availability_zone: " + rack + ","
-            #statsd.increment(metricName, tags=[metricTags])
-            api.Event.create(title="kafdrop-metrics", text=metricName, tags=metricTags)
-            logging.info(metricTags)
-            time.sleep(3)
-            
-        for idx, metricDict in brokerMetrics.items():
-            brokerId = brokerMetrics[idx]["brokerId"]
-            partitionsReplicas = brokerMetrics[idx]["partitionsReplicas"]
-            host = brokerMetrics[idx]["host"]
-            rack = brokerMetrics[idx]["rack"]
-
-            metricName = "aws.kafka.kafdrop.broker_partition_replicas:"
-            metricTags = "replicas_number:" + str(partitionsReplicas) + ", broker_id: " + brokerId + ", cluster_hostname: " + host + ", availability_zone: " + rack + ","
-            logging.info(metricName + metricTags)
-            #statsd.increment(metricName, tags=[metricTags])
-            api.Event.create(title="kafdrop-metrics", text=metricName, tags=metricTags)
-            logging.info(metricTags)
-            time.sleep(3)
-    else: 
-        logging.error("No metrics were found!")
-
-
-def printMessagesByTopicMetric():
-    try:
-        topics = requests.get(kafdropAddress +  "topic")
-        allTopics = json.loads(topics.text)
-
-        if allTopics:
-            for topic in allTopics:
-                topicName = str(topic['name'])
-                if not(topicName.startswith("__")):
-                    topicName = str(topic['name'])
-                    requestURL = kafdropAddress +  "topic/" + topicName + "/messages"
-                    messagesJson = requests.get(requestURL, headers={'accept': 'application/json'})
-                    messagesText = json.loads(messagesJson.text)
-                    lastOffSet = messagesText[0]['lastOffset']
-
-                    metricName = "aws.kafka.kafdrop.topic_messages"
-                    metricTags = metricExpression + str(lastOffSet) + ", topic_name: " + topicName + ", cluster_hostname: " + "" + ", availability_zone: " + "" + ","
-                    api.Event.create(title="kafdrop-metrics", text=metricName, tags=metricTags)
-                    logging.info(metricTags)
-
-        else:
-            logging.warning("No topics were found!")
-
-    except:
-        logging.error("It was not possible to connect with the MSK cluster.")
-        exit()
-
-def getTopicConsumers():
-    try:
-        topics = requests.get(kafdropAddress +  "topic")
-        allTopics = json.loads(topics.text)
-
-        if allTopics:
-            for topic in allTopics:
-                topicName = str(topic['name'])
-
-                if not(topicName.startswith("__")):
-                    requestURL = kafdropAddress +  "topic/" + topicName + "/consumers"
-                    topicConsumersJson = requests.get(requestURL, headers={'accept': 'application/json'})
-                    topicConsumersText = json.loads(topicConsumersJson.text)
-                    logging.info(topicConsumersText)
-
-        else:
-            logging.warning("No topics were found!")
-    
     except:
         logging.error("It was not possible to connect with the MSK cluster.")
         exit()
 
 def main():
     defineLogConfiguration()
-    readAllBrokers()
-    readTopicsAndPartitions()
-    printMetrics()
-    printMessagesByTopicMetric()
-    #getTopicConsumers()
-
+    loadTopicMessagesMetric()
+    
 if __name__ == "__main__":
     main()
